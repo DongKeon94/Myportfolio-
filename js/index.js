bf13d5d8ee42226a45b2483aa77a4245e63c8294
DongKeon94 <compuer1@naver.com>
2022-06-13 17:44:33 +0900

    6/13

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
-$(function(){
+$(document).ready(function(){
+
+    // Hello, World 텍스트
     $("#text2").hide();
 
-    $(".hello").on("mouseenter", function(){
-        $(".hello-text #text1").hide()
-        $(".hello-text #text2").fadeIn(1000)
-        $(".hello-text #text2").css({
-            "color" : "blue",
-        }, 1000, "swing");
+    $('.hello').on('mouseenter', function(){
+        $('.hello-text').css({'background':'#4169E1', 'filter':'drop-shadow(0px 4px 0px #191970)'});
+        $('.balloon_triangle').css({'border-top-color':'#4169E1', 'filter':'drop-shadow(0px 4px 0px #191970)'});
+        $('#text1').css('display','none');
+        $('#text2').css({'display':'block', 'color':'#fff'});
     });
-});
 
-$(function(){
-    $(".hello").on("mouseleave", function(){
-        $(".hello-text #text2").hide()
-        $(".hello-text #text1").fadeIn(1000)
+    $('.hello').on('mouseleave', function(){
+        $('.hello-text').css({'background':'#fff', 'filter':'drop-shadow(0px 4px 0px rgba(255,255,255,0.7))'});
+        $('.balloon_triangle').css({'border-top-color':'#fff', 'filter':'drop-shadow(0px 4px 0px rgba(255,255,255,0.7))'});
+        $('#text1').css('display','block');
+        $('#text2').css('display','none');
         
     });
-});
+
+})
+
+
